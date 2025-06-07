9bb4d78bbb43db9a9bebd5d617b59ccc0d0fefef
Anuj Sehrawat <143939648+anuj-sehrawat1@users.noreply.github.com>
2025-06-07 15:24:43 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
@@ -12,16 +12,14 @@ export default async function handler(req, res) {
 
     const googleUrl = `https://www.google.com/s?gl=in&gs_ri=maps&suggest=p&tbm=map&q=${encodedQuery}&pb=!2i15!4m8!1m3!1d1702.7860451264771!2d76.82201819999999!3d30.651211700000044!3m2!1i360!2i381!4f13.1!10b1!22m5!7e140!9sqvtDaKjFMZ-d4-EP2-HV8AI%3A78453932291!17sqvtDaKjFMZ-d4-EP2-HV8AI%3A78453932292!24m1!2e1!23m2!1e108!10b1`;
 
-    const googleResp = await fetch(googleUrl, {
+    const googleResp = await axios.get(googleUrl, {
       headers: {
         'User-Agent': 'Mozilla/5.0',
         'Accept': '*/*'
       }
     });
 
-    const result = await googleResp.text(); // or .json() if it's JSON
-
-    res.status(200).send(result);
+    res.status(200).send(googleResp.data);
 
   } catch (err) {
     console.error('Error:', err.message);
