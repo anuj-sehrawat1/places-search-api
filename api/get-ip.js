a63b471dab02b27dcff11e6fbab7d578009ffe3d
Anuj <143939648+anuj-sehrawat1@users.noreply.github.com>
2026-01-13 00:00:30 +0530

    Delete api/get-ip.js

--- api/get-ip.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const ip =
-    req.headers['x-forwarded-for']?.split(',')[0] ||
-    req.connection?.remoteAddress ||
-    req.socket?.remoteAddress;
-
-  res.status(200).json({ ip });
-}
